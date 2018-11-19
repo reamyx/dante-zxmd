0288f786b47e009e3e2d0d3d0ee03ba10827b670
zhixia <reamyx@126.com>
2018-11-19 21:20:33 +0800

    修改外部账号查询程序的调用方法,使用单一JSON格式作为调用参数

--- sockd/auth_password.c
+++ sockd/auth_password.c
@@ -51,10 +51,15 @@
 #include <sys/wait.h>
 #include <syslog.h>
 #include <unistd.h>
-#define BFSIZE 256
-#define IDSIZE 16
+#include <ctype.h>
 
-const char *expandpath="./skdpwdck.sh";
+#define BFSIZE 255
+#define PMSIZE 1023
+#define UUIDSZ 39
+#define UUIDFL "/proc/sys/kernel/random/uuid"
+#define UUIDCM "uuidgen"
+
+const char *expandpath="./skdaccountck.sh";
 
 static const char rcsid[] =
 "$Id: auth_password.c,v 1.41.6.2 2017/01/31 08:17:38 karls Exp $";
@@ -62,7 +67,7 @@ static const char rcsid[] =
 
 /* 通过指定的外部程序获取密码返回,同时传递用户提供的密码用于外部验证*/
 static const char *
-sockd_getpasswordhash(const char *login, char *pw, const size_t pwsize,
+sockd_getpassword(const char *login, char *pw, const size_t pwsize,
                              char *emsg, const size_t emsglen,
                              const char *uspwd, const char *path);
 /*
@@ -120,7 +125,7 @@ passwordcheck(name, cleartextpw, emsg, emsglen)
 
    /* usually need privileges to look up the password. */
    sockd_priv(SOCKD_PRIV_FILE_READ, PRIV_ON);
-   p = sockd_getpasswordhash(name,
+   p = sockd_getpassword(name,
                              pwhash,
                              sizeof(pwhash),
                              emsg,
@@ -145,23 +150,29 @@ passwordcheck(name, cleartextpw, emsg, emsglen)
    return rc;
 }
 
-/* 通过指定的外部程序获取密码并转换成HASH */
+
+/* 通过指定的外部程序获取密码并复制到密码存储区 */
 static const char *
-sockd_getpasswordhash(const char *login, char *pw, const size_t pwsize, char *emsg,
+sockd_getpassword(const char *login, char *pw, const size_t pwsize, char *emsg,
                       const size_t emsglen, const char *uspwd, const char *path) {
-    char pwbuff[BFSIZE+1], *desc, *sp, skdpid[IDSIZE], visstring[MAXNAMELEN * 4], *pathvis;
-	int p[2], kid, kst, readbytes = 0, readok = 0; 
-    void (*khd)(int) = NULL;
+
+	int p[2], kid, kst, pid, readbytes = 0, readok = 0; void (*khd)(int) = NULL;
+    char pwbuff[BFSIZE+1], *desc, *sp, visstring[MAXNAMELEN * 4], *pathvis,
+         *argv[3], parm[PMSIZE+1], j_login[MAXNAMELEN*2], j_uspwd[MAXPWLEN*2],
+         asessid[UUIDSZ+1];
+    
+    str_get_uuid(asessid, sizeof(asessid));
+    
     
     // 重置数据缓存,获取当前进程PID
-	memset(pwbuff, 0, BFSIZE+1); desc = pwbuff + BFSIZE;
-    snprintf(skdpid, sizeof(skdpid), "%u", *sockscf.state.motherpidv);
+	memset(pwbuff, 0, sizeof(pwbuff)); desc = pwbuff + BFSIZE;
+    pid = *sockscf.state.motherpidv;
     pathvis=str2vis(path, strlen(path), visstring, sizeof(visstring));
     
 	// 路径配置错误(未配置路径或目标不可执行),管道资源错误
-	if (path[0] == 0 || access(path, X_OK) < 0) {
-        snprintf(emsg, emsglen, "External program path config error: %s", pathvis); return NULL;}
-	if (pipe(p)) {snprintf(emsg, emsglen, "Fail to create a pipe for %s", pathvis); return NULL;}
+	if (access(path, X_OK) < 0) {
+        snprintf(emsg, emsglen, "External program execute error: %s", pathvis); return NULL;}
+	if (pipe(p)) {snprintf(emsg, emsglen, "Fail to create pipe for %s", pathvis); return NULL;}
     
 	// FORK子进程失败
 	khd = signal(SIGCHLD, SIG_DFL);
@@ -170,13 +181,21 @@ sockd_getpasswordhash(const char *login, char *pw, const size_t pwsize, char *em
     
 	// 子进程: 执行外部程序并通过父进程的读取管道提供目标数据
 	if (!kid) {
-		// 相关资源初始化,重定向标准输出,"NOIPPARAM"
+		// 相关资源初始化,重定向标准输出,
 		close(p[0]); closelog(); seteuid(getuid()); setegid(getgid());
 		if(dup2(p[1], 1) < 0) _exit(126); close(p[1]);
-		// 配置参数并运行程序: 用户名称 用户提供的密码或摘要 主进程PID ipparam
-		char *argv[7]; argv[0] = path; argv[1] = "SKDPW"; argv[2] = login;
-        argv[3] = uspwd; argv[4] = "NOIPPARAM"; argv[5] = skdpid; argv[6] = NULL;
-		execv(path, argv); _exit(127); }
+        // 构造JSON格式的参数递交至外部程序
+        str_tm_sncp(j_login, sizeof(j_login), login, '\"');
+        str_tm_sncp(j_uspwd, sizeof(j_uspwd), uspwd, '\"');
+        snprintf(parm, sizeof(parm),
+            "{ %s, %s, %s%s%s, %s%s%s, %s%u%s, %s%s%s }",
+            "\"method\": \"SKDPW\"", "\"srvname\": \"SOCKD\""
+            "\"usercnm\": \"",   j_user,      "\"",
+            "\"usercpw\": \"",   j_peerpwd,   "\"",
+            "\"srvpid\": \"",    pid,         "\"",
+            "\"asessid\": \"",   asessid,     "\"");
+        argv[0] = path; argv[1] = parm; argv[2] = NULL;
+        execv(path, argv); _exit(127); }
     
 	// 主程序: 从管道读取外部程序的标准输出,首行明文密码,次行描述信息
 	close(p[1]);
@@ -202,8 +221,30 @@ sockd_getpasswordhash(const char *login, char *pw, const size_t pwsize, char *em
     // 子程返回非0时返回错误
     if (WEXITSTATUS(kst)) {
         snprintf(emsg, emsglen, "Expand program exit whit code: %u, Error message: %s",
-        WEXITSTATUS(kst), str2vis(desc, strlen(desc), visstring, sizeof(visstring))); return NULL;}    
+        WEXITSTATUS(kst), str2vis(desc, strlen(desc), visstring, sizeof(visstring))); return NULL;}
     
     // 复制密码到缓存区后返回
-    snprintf(pw, pwsize, "%s", pwbuff); memset(pwbuff, 0, BFSIZE+1); return pw;}
+    snprintf(pw, pwsize, "%s", pwbuff); memset(pwbuff, 0, sizeof(pwbuff)); return pw; }
+
+// 通过内核或命令生成UUID字串,格式化并安全复制到目标区,返回目标串长度或-1
+static int str_get_uuid(char *dst, size_t dstlen) {
+    if (dstlen < 1) return -1;
+    char uuid[UUIDSZ+1], uuidfl[] = UUIDFL, uuidcm[]= UUIDCM,
+         *sp, *tsp = uuid + sizeof(uuid), *dp = dst, *tdp = dst + dstlen;
+    FILE *fp, *knl; memset(uuid, '\0', sizeof(uuid)); memset(dst, '\0', dstlen);
+    // 尝试读取kernel相关文件或执行系统命令生成UUID串
+    if ((knl = fp = fopen(uuidfl, "r")) || (fp = popen(uuidcm, "r"))) {
+        fgets(uuid, sizeof(uuid), fp); knl ? fclose(fp) : pclose(fp); }
+    // 执行字串格式化(清除换行和连接符)后安全复制到缓存区
+    while (sp = memchr(uuid, '\n', sizeof(uuid))) *sp = '\0';
+    while (sp = memchr(uuid, '-',  sizeof(uuid))) *sp = '\0';
+    for (sp = uuid; sp < tsp && dp < tdp; sp++) if (*sp) *dp++ = toupper(*sp);
+    if (dp == tdp) dp--; *dp = '\0'; return (int)(dp-dst); }
+
+// 将源字符串安全复制到目标区域并对指定字符添加转义符,返回目标串长度或-1
+static int str_tm_sncp(char *dst, size_t dstlen, char * src, char sc) {
+    if (dstlen < 1) return -1; memset(dst, '\0', dstlen);
+    char tm = 'A', *dp = dst, *tdp = dst + dstlen, *sp = src;
+    while (*sp && dp < tdp) *dp++ = (*sp == sc && tm)?(tm = '\0', '\\'):(tm = *sp++);
+    if (dp == tdp) dp--; *dp = '\0'; return (int)(dp-dst); }
 
